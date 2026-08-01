e7e60a5dec42d0cef8c4441f51c35da4a1e54e61
Rafi-Sojan <stronggamer5645@gmail.com>
2026-08-01 09:14:22 +0530

    organized file structure

---
+++ SNES-Emulator/include/src/emulator-main.cpp
@@ -0,0 +1,9 @@
+﻿
+#include "emulator.h"
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
---
+++ SNES-Emulator/include/src/emulator.h
@@ -0,0 +1,10 @@
+﻿
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <math.h>
