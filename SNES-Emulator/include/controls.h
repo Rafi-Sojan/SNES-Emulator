1977f61c957865d553bc0424f030f58859da3bac
Rafi-Sojan <stronggamer5645@gmail.com>
2026-08-23 04:14:00 +0530

    introduced class for controls and tools, declared the required functions, the controls can be changed by the user as per their needs

---
+++ SNES-Emulator/include/controls.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <fstream>
+#include <array>
+#include <filesystem>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+
+class Joypad {
+public:
+
+	// these are my preferrable controller settings i use for playing most of the games in snes, preferrably for Super Metroid  you can change the controls as you wish  
+	Joypad() = default;
+	void movementinput() {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+			d_up();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+			d_down();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+			d_right();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+			d_left();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z))
+			d_X();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::X))
+			d_Y();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::C))
+			d_A();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::V))
+			d_B();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+			d_RS();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+			d_LS();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Enter))
+			d_Start();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LShift))
+			d_Select();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::I))
+			increasebrightness();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::O))
+			decreasebrightness();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::P))
+			forceblank();
+	}
+
+private:
+	void d_up();
+	void d_down();
+	void d_left();
+	void d_right();
+	void increasebrightness();
+	void decreasebrightness();
+	void d_A();
+	void d_B();
+	void d_Y();
+	void d_X();
+	void d_LS();
+	void d_RS();
+	void d_Start();
+	void d_Select();
+	void forceblank();
+};
---
+++ SNES-Emulator/src/controls.cpp
@@ -0,0 +1,53 @@
+#include "../include/controls.h"
+
+void Joypad::d_up() {
+
+}
+
+void Joypad::d_down() {
+
+}
+
+void Joypad::d_right() {
+
+}
+
+void Joypad::d_left() {
+
+}
+
+void Joypad::d_Select() {
+
+}
+
+void Joypad::d_Start() {
+
+}
+
+void Joypad::d_A() {
+
+}
+
+void Joypad::d_B() {
+
+}
+
+void Joypad::d_X() {
+
+}
+
+void Joypad::d_Y() {
+
+}
+
+void Joypad::forceblank() {
+
+}
+
+void Joypad::increasebrightness() {
+
+}
+
+void Joypad::decreasebrightness() {
+
+}
